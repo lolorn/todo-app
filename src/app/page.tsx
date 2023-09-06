202a8ee33525ddc97f4b9e1af1abc15ab4944481
Mavvven <137602034+lolorn@users.noreply.github.com>
2023-09-06 09:49:54 +0800

    前端开始

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import { createUser } from "../../prisma/user";
-
 export default function Home() {
-  const status: number = 1;
   return (
     <>
       <div className="text-2xl">Todo Project</div>
